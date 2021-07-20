1eeabd3c695a005ce684e19d2baf4cabcc7c3398
Victor Yanev <nvickom01@gmail.com>
2021-07-20 21:25:59 +0300

    Code reformatting

--- angle_finder.py
+++ angle_finder.py
@@ -11,35 +11,13 @@ avg_dist = 0
 avg_dist_n = 0
 
 
-def get_min_distance_cnt_index(coords, remaining_cnts, remaining_coords):
-    min_dist = None
-    min_index = -1
-    for i in range(0, len(remaining_cnts)):
-        dist = get_distance(coords, remaining_coords[i])
-        if min_dist is None or dist < min_dist:
-            min_dist = dist
-            min_index = i
-    return min_index
-
-
 def get_distance(pt1: (int, int), pt2: (int, int)):
-    """Returns the Euclidean distance between this point and that point.
-    :param pt1: (x: int, y: int)
-    :param pt2: (x: int, y: int)
-    :return: the distance between pt1 and pt2
-    """
     a = pt2[0] - pt1[0]
     b = pt2[1] - pt1[1]
     return math.sqrt(a**2 + b**2)
 
 
 def double_area(a: (int, int), b: (int, int), c: (int, int)):
-    """Returns twice the signed area of the triangle a-b-c.
-    :param a: ((int, int)): first point
-    :param b: ((int, int)): second point
-    :param c: ((int, int)): third point
-    :return: Twice the signed area of the triangle a-b-c
-    """
     return (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
 
 
@@ -56,6 +34,17 @@ def update_avg_dist(new_dist):
     avg_dist_n += 1
 
 
+def get_min_distance_cont_index(coords, remaining_conts, remaining_coords):
+    min_dist = None
+    min_index = -1
+    for i in range(0, len(remaining_conts)):
+        dist = get_distance(coords, remaining_coords[i])
+        if min_dist is None or dist < min_dist:
+            min_dist = dist
+            min_index = i
+    return min_index
+
+
 def sort_contours(conts, sort_by_distance=False, sort_by_top_left=False):
     if sort_by_top_left:
         bounding_boxes = [cv2.boundingRect(cont) for cont in conts]
@@ -68,7 +57,7 @@ def sort_contours(conts, sort_by_distance=False, sort_by_top_left=False):
         for j in range(len(conts) - 1):
             distance = get_distance(coords[j], coords[j + 1])
             if distance > 75:
-                offset = get_min_distance_cnt_index(coords[j], conts[j + 2:], coords[j + 2:])
+                offset = get_min_distance_cont_index(coords[j], conts[j + 2:], coords[j + 2:])
                 if offset != -1:
                     next_cnt_index = j + 2 + offset
                     conts[j + 1], conts[next_cnt_index] = conts[next_cnt_index], conts[j + 1]
@@ -111,41 +100,41 @@ def get_curvature_and_positional_data() -> (pd.DataFrame, pd.DataFrame):
             for image_name in os.listdir(exp_path):
                 img_path = os.path.join(exp_path, image_name)
 
-                # load the image
+                # Load the image
                 img = cv2.imread(img_path)
 
-                # crop the image if it's from the second set
+                # Crop the image if it's from the second set
                 if exp_date == "SoftAct11-06-21":
                     margin = 200
                     img = img[margin:-margin, margin:-margin]
 
-                # apply contrast
+                # Apply contrast
                 alpha = 1.1  # Contrast control (1.0-3.0)
                 beta = -30  # Brightness control (0-100)
                 img = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
 
-                # image size = 1/2 * original size
+                # Set image size = 1/2 * original size
                 height, width, _ = img.shape
                 new_height, new_width = int(height / 2), int(width / 2)
                 img = cv2.resize(img, (new_width, new_height))
 
-                # set lower and upper range for the green marker
+                # Set lower and upper range for the green marker
                 lower_range = np.array([70, 50, 90])
                 upper_range = np.array([100, 255, 240])
 
-                # get only the marker areas from the image using a color threshold
+                # Get only the marker areas from the image using a color threshold
                 mask = cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), lower_range, upper_range)
 
-                # dilate mask
+                # Dilate mask
                 kernel = np.ones((5, 5), np.uint8)
                 mask = cv2.dilate(mask, kernel, iterations=1)
 
-                # find contours in the thresholded image
+                # Find contours in the thresholded image
                 cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                 cnts = list(filter(lambda cnt: cv2.contourArea(cnt) > 50, cnts))
                 cnts = sort_contours(cnts, sort_by_distance=True, sort_by_top_left=True)
 
-                # get the middle points of all marker areas
+                # Get the middle points of all marker areas
                 blank_img = np.zeros((new_height, new_width, 3), np.uint8)
                 i = 0
                 d = []
@@ -171,9 +160,11 @@ def get_curvature_and_positional_data() -> (pd.DataFrame, pd.DataFrame):
                 # Draw the fitted circle on the image
                 cv2.circle(blank_img, (int(fitted_circle[0][0]), int(fitted_circle[0][1])), int(fitted_circle[1]), (255,255, 255), 1)
 
+                # Extract X and Y coordinates
                 coords_X, coords_Y = df['X-Value'].tolist(), df['Y-Value'].tolist()
                 print(image_name, coords_X, coords_Y)
 
+                # Calculate the curvatures
                 curvatures = []
                 for i in range(0, 12, 3):
                     a = (coords_X[i], coords_Y[i])
@@ -181,6 +172,7 @@ def get_curvature_and_positional_data() -> (pd.DataFrame, pd.DataFrame):
                     c = (coords_X[i + 2], coords_Y[i + 2])
                     curvatures.append(menger_curvature(a, b, c))
 
+                # Fill the dataframes with the extracted positional and curvature data
                 curvature_d.append({
                     "image": image_name,
                     **{f"curvature {i+1}": curvatures[i] for i in range(len(curvatures))}
@@ -192,7 +184,7 @@ def get_curvature_and_positional_data() -> (pd.DataFrame, pd.DataFrame):
                     ** {f"chamber {i+1} Y": coords_Y[i] - coords_Y[0] for i in range(2, 12)}
                 })
 
-                # # Debugging:
+                # Debugging:
                 # cv2.imshow('Image', img)                                      # TODO: Remove this later
                 # cv2.imshow('Mask', mask)                                      # TODO: Remove this later
                 # cv2.imshow(f"{image_name} - dots", blank_img)                 # TODO: Remove this later
@@ -202,7 +194,11 @@ def get_curvature_and_positional_data() -> (pd.DataFrame, pd.DataFrame):
 
 if __name__ == '__main__':
     curvature_data, positional_data = get_curvature_and_positional_data()
+
     print(f"Average distance between {avg_dist_n} chambers: {avg_dist}")
-    print(curvature_data)
-    print(positional_data)
+
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.expand_frame_repr', False):
+        print(curvature_data)
+        print(positional_data)
+
     cv2.destroyAllWindows()
--- lstm_train.py
+++ lstm_train.py
@@ -21,89 +21,90 @@ def split_sequence(sequence, n_steps):
     return np.asarray(x)
 
 
-def split_train_test(X_in: np.ndarray, Y_in: np.ndarray, train_p: float):
+def split_train_test(X_in: np.ndarray, y_in: np.ndarray, train_p: float):
     train_size = int(X_in.size * train_p)
 
     train_X, test_X = X_in[:train_size], X_in[train_size:]
-    train_Y, test_Y = Y_in[:train_size], Y_in[train_size:]
+    train_Y, test_Y = y_in[:train_size], y_in[train_size:]
 
     return train_X, test_X, train_Y, test_Y
 
 
-# load the dataset
-curv_and_pos_dfs = get_combined_data()
+if __name__ == "__main__":
+    # load the dataset
+    curv_and_pos_dfs = get_combined_data()
 
-df_num = 0
-for df_num in (2, 1):
-    df = curv_and_pos_dfs[df_num - 1]
+    df_num = 0
+    for df_num in (2, 1):
+        df = curv_and_pos_dfs[df_num - 1]
 
-    # split into samples
-    X, y = split_input_output(df)
-    print(X.shape, y.shape)
+        # split into samples
+        X, y = split_input_output(df)
+        print(X.shape, y.shape)
 
-    # split into train/test
-    X_train, X_test, y_train, y_test = split_train_test(X, y, 0.8)
+        # split into train/test
+        x_train, x_test, y_train, y_test = split_train_test(X, y, 0.8)
 
-    # reshape into (n_samples, time_steps, features)
-    X_train = X_train.reshape(X_train.shape[0], 1, 1)
-    X_test = X_test.reshape(X_test.shape[0], 1, 1)
-    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+        # reshape into (n_samples, time_steps, features)
+        x_train = x_train.reshape(x_train.shape[0], 1, 1)
+        x_test = x_test.reshape(x_test.shape[0], 1, 1)
+        print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
 
-    # define model
-    model = Sequential()
-    model.add(LSTM(250, activation='relu', kernel_initializer='he_normal', input_shape=(X_train.shape[1], 1)))
-    model.add(Dense(200, activation='relu', kernel_initializer='he_normal'))
-    model.add(Dense(150, activation='relu', kernel_initializer='he_normal'))
-    model.add(Dense(100, activation='relu', kernel_initializer='he_normal'))
-    model.add(Dense(50, activation='relu', kernel_initializer='he_normal'))
-    model.add(Dense(y_train.shape[1]))
+        # define model
+        model = Sequential()
+        model.add(LSTM(250, activation='relu', kernel_initializer='he_normal', input_shape=(x_train.shape[1], 1)))
+        model.add(Dense(200, activation='relu', kernel_initializer='he_normal'))
+        model.add(Dense(150, activation='relu', kernel_initializer='he_normal'))
+        model.add(Dense(100, activation='relu', kernel_initializer='he_normal'))
+        model.add(Dense(50, activation='relu', kernel_initializer='he_normal'))
+        model.add(Dense(y_train.shape[1]))
 
-    # compile the model
-    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+        # compile the model
+        model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
-    # fit the model
-    model.fit(X_train, y_train, epochs=500, batch_size=32, verbose=2, validation_data=(X_test, y_test))
+        # fit the model
+        model.fit(x_train, y_train, epochs=500, batch_size=32, verbose=2, validation_data=(x_test, y_test))
 
-    # evaluate the model
-    mse, mae = model.evaluate(X_test, y_test, verbose=0)
-    print('MSE: %.3f, RMSE: %.3f, MAE: %.3f' % (mse, np.sqrt(mse), mae))
+        # evaluate the model
+        mse, mae = model.evaluate(x_test, y_test, verbose=0)
+        print('MSE: %.3f, RMSE: %.3f, MAE: %.3f' % (mse, np.sqrt(mse), mae))
 
-    if df_num == 1:
-        columns = [f"curvature {i+1}" for i in range(4)]
-    else:
-        columns = [f"chamber {j+1}" for j in range(2, 12)]
-
-    pressures = [0, 1, 2, 3, 4]
-    predictions = pd.DataFrame()
-    for pressure_val in pressures:
-        pressure_row = np.asarray([[pressure_val]])
-        pressure_row = pressure_row[0].reshape(pressure_row.shape[0], 1, 1)
-        what: np.ndarray = model.predict(pressure_row)
         if df_num == 1:
-            prediction = np.concatenate(([pressure_val], what.flatten())).reshape(what.shape[0], what.shape[1] + 1)
-            predictions = predictions.append(pd.DataFrame(prediction, columns=["pressure_val"] + columns))
-        elif df_num == 2:
-            X_coords_predicted, Y_coords_predicted = split_two_halves(what)
-            all_coords = [(int(x), int(y)) for x, y in list(zip(X_coords_predicted, Y_coords_predicted))]
+            columns = [f"curvature {i+1}" for i in range(4)]
+        else:
+            columns = [f"chamber {j+1}" for j in range(2, 12)]
 
-            prediction = np.asarray([pressure_val] + all_coords, dtype=object).reshape(1, len(all_coords) + 1)
-            predictions = predictions.append(pd.DataFrame(prediction, columns=["pressure_val"] + columns))
+        pressures = [0, 1, 2, 3, 4]
+        predictions = pd.DataFrame()
+        for pressure_val in pressures:
+            pressure_row = np.asarray([[pressure_val]])
+            pressure_row = pressure_row[0].reshape(pressure_row.shape[0], 1, 1)
+            what: np.ndarray = model.predict(pressure_row)
+            if df_num == 1:
+                prediction = np.concatenate(([pressure_val], what.flatten())).reshape(what.shape[0], what.shape[1] + 1)
+                predictions = predictions.append(pd.DataFrame(prediction, columns=["pressure_val"] + columns))
+            elif df_num == 2:
+                X_coords_predicted, Y_coords_predicted = split_two_halves(what)
+                all_coords = [(int(x), int(y)) for x, y in list(zip(X_coords_predicted, Y_coords_predicted))]
 
-            X_coords_actual, Y_coords_actual = split_two_halves(y[pressure_val])
+                prediction = np.asarray([pressure_val] + all_coords, dtype=object).reshape(1, len(all_coords) + 1)
+                predictions = predictions.append(pd.DataFrame(prediction, columns=["pressure_val"] + columns))
 
-            init_X = 800
-            init_Y = 100
-            blank_img = np.zeros((720, 1280, 3), np.uint8)
-            for i in range(len(X_coords_predicted)):
-                cX_p = int(X_coords_predicted[i] + init_X)
-                cY_p = int(Y_coords_predicted[i] + init_Y)
-                cX_a = int(X_coords_actual[i] + init_X)
-                cY_a = int(Y_coords_actual[i] + init_Y)
-                cv2.circle(blank_img, (cX_p, cY_p), 1, (255, 255, 255), -1)
-                cv2.circle(blank_img, (cX_a, cY_a), 1, (125, 125, 125), -1)
-            cv2.imshow(f"predicted position", blank_img)
-            cv2.waitKey(0)
+                X_coords_actual, Y_coords_actual = split_two_halves(y[pressure_val])
 
-    print('Predictions: ')
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.expand_frame_repr', False):
-        print(predictions)
+                init_X = 800
+                init_Y = 100
+                blank_img = np.zeros((720, 1280, 3), np.uint8)
+                for i in range(len(X_coords_predicted)):
+                    cX_p = int(X_coords_predicted[i] + init_X)
+                    cY_p = int(Y_coords_predicted[i] + init_Y)
+                    cX_a = int(X_coords_actual[i] + init_X)
+                    cY_a = int(Y_coords_actual[i] + init_Y)
+                    cv2.circle(blank_img, (cX_p, cY_p), 1, (255, 255, 255), -1)
+                    cv2.circle(blank_img, (cX_a, cY_a), 1, (125, 125, 125), -1)
+                cv2.imshow(f"predicted position", blank_img)
+                cv2.waitKey(0)
+
+        print('Predictions: ')
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.expand_frame_repr', False):
+            print(predictions)
